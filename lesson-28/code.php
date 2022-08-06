4b37a077f1d11ef47ab6139c6c615c3114670261
Deepmarik <deepmarik@gmail.com>
2022-08-06 12:08:58 +0300

    created folder less-28

--- lesson-28/code.php
+++ lesson-28/code.php
@@ -1,15 +1,10 @@
 <?php
-	//⊗ppPmArFl
-	//урок 27 
+	//⊗ppPmArVK
+	//урок 28 
 	 
 	//Задание 1
 
-	$arr = [];
-	$arr[] = 1;
-	$arr[] = 2;
-	$arr[] = 3;
-	$arr[] = 4;
-	$arr[] = 5;
+	$arr = [1, 2, 3, 4, 5];
 	var_dump ($arr);
 	echo PHP_EOL;
 
