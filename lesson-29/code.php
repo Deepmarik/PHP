db1a4c01f82761fda42acdd2bd72eeb881de58d7
Deepmarik <deepmarik@gmail.com>
2022-08-07 00:52:33 +0300

    created folder lesson-29

---
+++ lesson-29/code.php
@@ -0,0 +1,20 @@
+<?php
+	//⊗ppPmCdIE
+	//урок 29 
+	 
+	//Задание 1
+
+	$arr = [1, 2, 3, 4, 5];
+	$key1 = 1;
+	$key2 = 2;
+	echo $arr[$key1] + $arr[$key2];
+	echo PHP_EOL;
+
+	//Задание 2
+
+	$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+	$key1 = 'b';
+	echo $arr[$key1];
+	echo PHP_EOL;
+		
+?>
