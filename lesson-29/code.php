3cc10bcd77c2756ca6c0472b689fb44059bdc477
Deepmarik <deepmarik@gmail.com>
2022-08-07 01:19:30 +0300

    lesson-29 is done

--- lesson-29/code.php
+++ lesson-29/code.php
@@ -4,17 +4,59 @@
 	 
 	//Задание 1
 
-	$arr = [1, 2, 3, 4, 5];
-	$key1 = 1;
-	$key2 = 2;
-	echo $arr[$key1] + $arr[$key2];
+	$test = 12;
+	if ($test > 10) {
+		echo '+';
+	} 	else { echo '-';}
 	echo PHP_EOL;
 
 	//Задание 2
 
-	$arr = ['a' => 1, 'b' => 2, 'c' => 3];
-	$key1 = 'b';
-	echo $arr[$key1];
+	$test = 19;
+	if ($test < 10) {
+		echo '+';
+	} 	else { echo '-';}
+	echo PHP_EOL;
+
+	//Задание 3
+
+	$test = 10;
+	if ($test <= 10) {
+		echo '+';
+	}	else { echo '-';}
 	echo PHP_EOL;
 		
+	//Задание 4
+
+	$test = 10;
+	if ($test >= 10) {
+		echo '+';
+	}	else { echo '-';}
+	echo PHP_EOL;
+
+	//Задание 5
+
+	$test = 10;
+	if ($test == 10) {
+		echo '+';
+	}	else { echo '-';}
+	echo PHP_EOL;
+
+	//Задание 6
+
+	$test = 10;
+	if ($test != 10) {
+		echo '+';
+	}	else { echo '-';}
+	echo PHP_EOL;
+
+	//Задание 7
+	$test1 = 1;
+	$test2 = 2;
+	
+	if ($test2 > $test1) {
+		echo 'test2 больше'; // сработает это, так как $test2 больше $test1
+	} else {
+		echo 'test1 больше';
+	}
 ?>
